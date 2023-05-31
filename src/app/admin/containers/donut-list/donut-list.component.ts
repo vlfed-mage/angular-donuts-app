69aa90fc57fd9c3c0a7eca2feda231fc52130457
Valdo <gkorchun@gmail.com>
2023-05-31 16:56:01 +0300

    ngFor and iterating collections

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -7,14 +7,28 @@ import { Donut } from '../../models/donut.model';
 @Component({
     selector: 'donut-list',
     template: `
-        <ng-container
-            *ngIf="donuts.length; then cards; else nothing"></ng-container>
+        <ng-container *ngIf="donuts.length; else nothing">
+            <donut-card
+                *ngFor="let donut of donuts; trackBy: trackById"
+                [donut]="donut"></donut-card>
 
-        <ng-template #cards>
-            <donut-card [donut]="donuts[0]"></donut-card>
-            <donut-card [donut]="donuts[1]"></donut-card>
-            <donut-card [donut]="donuts[2]"></donut-card>
-        </ng-template>
+            <!-- it's the same as -->
+            <!-- but we can pass other data deeper -->
+
+            <ng-template
+                ngFor
+                [ngForOf]="donuts"
+                [ngForTrackBy]="trackById"
+                let-donut
+                let-i="index"
+                let-o="odd"
+                let-e="even">
+                {{ i }}
+                {{ e }}
+                {{ o }}
+                <donut-card [donut]="donut"></donut-card>
+            </ng-template>
+        </ng-container>
         <ng-template #nothing>
             <p>Nothing is here...</p>
         </ng-template>
@@ -26,28 +40,32 @@ export class DonutListComponent implements OnInit {
 
     ngOnInit(): void {
         this.donuts = [
-            // {
-            //     id: 'y8z0As',
-            //     name: 'Just Chocolate',
-            //     icon: 'just-chocolate',
-            //     price: 119,
-            //     description: 'For the pure chocoholic.',
-            // },
-            // {
-            //     id: '3u98Kl',
-            //     name: 'Glazed Fudge',
-            //     icon: 'glazed-fudge',
-            //     price: 129,
-            //     promo: true,
-            //     description: 'Sticky perfection.',
-            // },
-            // {
-            //     id: 'ae098s',
-            //     name: 'Caramel Swirl',
-            //     icon: 'caramel-swirl',
-            //     price: 129,
-            //     description: 'Chocolate drizzled with caramel.',
-            // },
+            {
+                id: 'y8z0As',
+                name: 'Just Chocolate',
+                icon: 'just-chocolate',
+                price: 119,
+                description: 'For the pure chocoholic.',
+            },
+            {
+                id: '3u98Kl',
+                name: 'Glazed Fudge',
+                icon: 'glazed-fudge',
+                price: 129,
+                promo: true,
+                description: 'Sticky perfection.',
+            },
+            {
+                id: 'ae098s',
+                name: 'Caramel Swirl',
+                icon: 'caramel-swirl',
+                price: 129,
+                description: 'Chocolate drizzled with caramel.',
+            },
         ];
+    }
+
+    trackById(index: number, value: Donut) {
+        return value.id;
     }
 }
