4cdb139364031a51432b5355f98bec78b185d95f
Valdo <gkorchun@gmail.com>
2023-05-31 15:44:11 +0300

    Conditional rendering with ngIf else and then

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -7,9 +7,17 @@ import { Donut } from '../../models/donut.model';
 @Component({
     selector: 'donut-list',
     template: `
-        <donut-card [donut]="donuts[0]"></donut-card>
-        <donut-card [donut]="donuts[1]"></donut-card>
-        <donut-card [donut]="donuts[2]"></donut-card>
+        <ng-container
+            *ngIf="donuts.length; then cards; else nothing"></ng-container>
+
+        <ng-template #cards>
+            <donut-card [donut]="donuts[0]"></donut-card>
+            <donut-card [donut]="donuts[1]"></donut-card>
+            <donut-card [donut]="donuts[2]"></donut-card>
+        </ng-template>
+        <ng-template #nothing>
+            <p>Nothing is here...</p>
+        </ng-template>
     `,
     styles: [],
 })
@@ -18,28 +26,28 @@ export class DonutListComponent implements OnInit {
 
     ngOnInit(): void {
         this.donuts = [
-            {
-                id: 'y8z0As',
-                name: 'Just Chocolate',
-                icon: 'just-chocolate',
-                price: 119,
-                description: 'For the pure chocoholic.',
-            },
-            {
-                id: '3u98Kl',
-                name: 'Glazed Fudge',
-                icon: 'glazed-fudge',
-                price: 129,
-                promo: true,
-                description: 'Sticky perfection.',
-            },
-            {
-                id: 'ae098s',
-                name: 'Caramel Swirl',
-                icon: 'caramel-swirl',
-                price: 129,
-                description: 'Chocolate drizzled with caramel.',
-            },
+            // {
+            //     id: 'y8z0As',
+            //     name: 'Just Chocolate',
+            //     icon: 'just-chocolate',
+            //     price: 119,
+            //     description: 'For the pure chocoholic.',
+            // },
+            // {
+            //     id: '3u98Kl',
+            //     name: 'Glazed Fudge',
+            //     icon: 'glazed-fudge',
+            //     price: 129,
+            //     promo: true,
+            //     description: 'Sticky perfection.',
+            // },
+            // {
+            //     id: 'ae098s',
+            //     name: 'Caramel Swirl',
+            //     icon: 'caramel-swirl',
+            //     price: 129,
+            //     description: 'Chocolate drizzled with caramel.',
+            // },
         ];
     }
 }
