9fad4e2a118483e5cbd52bc55615d1f479f6134d
Valdo <gkorchun@gmail.com>
2023-06-15 17:14:02 +0300

    Refactoring for services and http requests

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 // containers
 import { DonutListComponent } from './containers/donut-list/donut-list.component';
@@ -11,13 +12,13 @@ import { DonutCardComponent } from './components/donut-card/donut-card.component
 import { DonutFormComponent } from './components/donut-form/donut-form.component';
 
 @NgModule({
-  declarations: [
-    DonutListComponent,
-    DonutSingleComponent,
-    DonutCardComponent,
-    DonutFormComponent,
-  ],
-  imports: [CommonModule, FormsModule],
-  exports: [DonutListComponent, DonutSingleComponent],
+    declarations: [
+        DonutListComponent,
+        DonutSingleComponent,
+        DonutCardComponent,
+        DonutFormComponent,
+    ],
+    imports: [CommonModule, FormsModule, HttpClientModule],
+    exports: [DonutListComponent, DonutSingleComponent],
 })
 export class AdminModule {}
--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -7,7 +7,7 @@ import { DonutService } from '../../services/donut.service';
     selector: 'donut-list',
     template: `
         <div>
-            <ng-container *ngIf="donuts.length; else nothing">
+            <ng-container *ngIf="donuts?.length; else nothing">
                 <donut-card
                     *ngFor="let donut of donuts; trackBy: trackById; index as i"
                     [donut]="donut">
@@ -27,7 +27,9 @@ export class DonutListComponent implements OnInit {
     constructor(private donutService: DonutService) {}
 
     ngOnInit(): void {
-        this.donuts = this.donutService.read();
+        this.donutService
+            .read()
+            .subscribe((donuts: Donut[]) => (this.donuts = donuts));
     }
 
     trackById(index: number, value: Donut) {
